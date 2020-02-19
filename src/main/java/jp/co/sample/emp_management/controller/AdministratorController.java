ea624110dc758044f70f77ccc44db237adc51c9d
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-02-19 16:56:01 +0900

    メールアドレス重複エラーの表示形式の修正

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class AdministratorController {
 
 	@Autowired
 	private AdministratorService administratorService;
-	
+
 	@Autowired
 	private HttpSession session;
 
@@ -41,8 +42,8 @@ public class AdministratorController {
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
-	
-	//  (SpringSecurityに任せるためコメントアウトしました)
+
+	// (SpringSecurityに任せるためコメントアウトしました)
 	@ModelAttribute
 	public LoginForm setUpLoginForm() {
 		return new LoginForm();
@@ -68,14 +69,14 @@ public class AdministratorController {
 	 * @return ログイン画面へリダイレクト
 	 */
 	@RequestMapping("/insert")
-	public String insert(@Validated InsertAdministratorForm form, BindingResult result, Model model) {
-		//入力値エラーとなった場合は登録画面へ遷移
-		if(result.hasErrors()) {
+	public String insert(@Validated InsertAdministratorForm form, BindingResult result) {
+		// 入力値エラーとなった場合は登録画面へ遷移
+		if (result.hasErrors()) {
 			return toInsert();
 		}
-		//メールアドレスから管理者情報を取得できる場合は登録画面へ遷移
-		if(administratorService.findByMailAddress(form.getMailAddress()) != null) {
-			model.addAttribute("mail_check_message", "メールアドレスが既に登録されています");
+		// メールアドレスから管理者情報を取得できる場合は登録画面へ遷移
+		if (administratorService.findByMailAddress(form.getMailAddress()) != null) {
+			result.addError(new FieldError(result.getObjectName(), "mailAddress", "メールアドレスが既に登録されています"));
 			return toInsert();
 		}
 		Administrator administrator = new Administrator();
@@ -101,10 +102,8 @@ public class AdministratorController {
 	/**
 	 * ログインします.
 	 * 
-	 * @param form
-	 *            管理者情報用フォーム
-	 * @param result
-	 *            エラー情報格納用オブッジェクト
+	 * @param form   管理者情報用フォーム
+	 * @param result エラー情報格納用オブッジェクト
 	 * @return ログイン後の従業員一覧画面
 	 */
 	@RequestMapping("/login")
@@ -116,7 +115,7 @@ public class AdministratorController {
 		}
 		return "forward:/employee/showList";
 	}
-	
+
 	/////////////////////////////////////////////////////
 	// ユースケース：ログアウトをする
 	/////////////////////////////////////////////////////
@@ -130,5 +129,5 @@ public class AdministratorController {
 		session.invalidate();
 		return "redirect:/";
 	}
-	
+
 }
